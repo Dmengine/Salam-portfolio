2ee932ad4b5a2bf3a63a0cca12966b3afd6008d6
Dmengine <azeezoladimeji23@gmail.com>
2023-07-16 11:37:23 +0100

    update

--- app/components/project.js
+++ app/components/project.js
@@ -1,14 +1,52 @@
-const project = [
+const projects = [
     {
-        id: 1,
-        name: 'Medfest',
-        PlayStore: 'https://play.google.com/store/apps/details?id=com.medfestcareng.medfestcare',
-        website: 'https://www.medfestcareng.com',
-        Project_Description: 'MedfestCare helps you; save money for health care, locate medical facilities around you, pay your medical bills from MedfestCare wallet, access free funds for health care especially for pregnant women, children under 5 years and the aged, help family and friends with funds for health care, get help funds from the MedfestCare community for healthcare and so much more.',
+      name: 'Medfest',
+      PlayStore: 'https://play.google.com/store/apps/details?id=com.medfestcareng.medfestcare',
+      link: 'https://www.medfestcareng.com',
+      details: 'MedfestCare helps you save money for healthcare, locate medical facilities around you, pay your medical bills from MedfestCare wallet, access free funds for healthcare (especially for pregnant women, children under 5 years, and the aged), help family and friends with funds for healthcare, get help funds from the MedfestCare community for healthcare, and so much more.',
+      images: [
+        "./assets/images/Medfest/Screenshot_20230707-011214.jpg",
+        "./assets/images/Medfest/Screenshot_20230707-011218.jpg",
+        "./assets/images/Medfest/Screenshot_20230707-011340.jpg",
+        "./assets/images/Medfest/Screenshot_20230707-011354.jpg",
+        "./assets/images/Medfest/Screenshot_20230707-011453.jpg",
+        "./assets/images/Medfest/Screenshot_20230707-011505.jpg",
+        "./assets/images/Medfest/Screenshot_20230707-011554.jpg",
+      ],
     },
     {
-        id: 2,
-        name: '',
-        
+      name: 'Poke Wallet',
+      link: 'https://pokmon.org/',
+      details: `
+        Pokewallet is a unique and innovative app that combines the excitement of memes with the power of a decentralized platform. With Pokewallet, users can easily send and receive coins and tokens, making transactions fast and secure. But that's not all - we've taken it a step further by incorporating access to the crypto market, giving users the opportunity to stay updated on the latest trends and make informed investment decisions.
+  
+        Key Features:
+        - Memes for Fun: Experience the joy of using crypto in a light-hearted and humorous way with our meme-based interface. It's a refreshing break from the traditional, dull financial applications.
+        - Decentralized Security: Rest assured that your transactions are secure and private, thanks to the decentralized nature of Pokewallet. Your funds remain in your control at all times.
+        - Seamless Coin & Token Transactions: Sending and receiving coins and tokens is a breeze with Pokewallet. No more complicated processes or delays - simply enjoy fast and hassle-free transactions.
+        - Stay Informed with Crypto Market Access: Empower yourself with real-time access to the crypto market. Keep track of price movements, discover new coins, and make educated decisions with the built-in market feature.
+  
+        Whether you're a seasoned crypto enthusiast or a beginner looking to dip your toes into the world of digital currencies, Pokewallet is the app that combines fun and functionality. Join the Pokewallet community today and embark on your crypto journey with a smile!
+      `,
+      images: [
+        "./assets/images/PokeWallet/Screenshot_20230707-020055.jpg",
+        "./assets/images/PokeWallet/Screenshot_20230707-020101.jpg",
+        "./assets/images/PokeWallet/Screenshot_20230707-020112.jpg",
+        "./assets/images/PokeWallet/Screenshot_20230707-020118.jpg",
+        "./assets/images/PokeWallet/Screenshot_20230707-020129.jpg",
+        "./assets/images/PokeWallet/Screenshot_20230707-020154.jpg",
+        "./assets/images/PokeWallet/Screenshot_20230707-020231.jpg",
+      ]
     },
-]
+    {
+        name: 'Radio Fm',
+        PlayStore: 'https://play.google.com/store/apps/details?id=com.akrodevelopers.radiofmam&hl=es_AR&gl=US',
+        details: `
+            This is an exceptional free online radio app that offers exclusive features. Now you can enjoy free live radio streaming without facing any problem.
+            This free android radio app turns your phone into a portable radio. This radio app comes with AM FM radio stations that you can access from anywhere in the world.
+        `
+    },
+  ];
+  
+  export default projects;
+  
